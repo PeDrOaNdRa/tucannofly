94029060a5d4a7932fc46e008f008744236a86ac
Pedro Henrique de Oliveira Andrade <pedro.andrade9@aluno.senai.br>
2023-08-07 12:12:08 -0300

    abhcdd

--- FlappyBird/Assets/Script/GameManager.cs
+++ FlappyBird/Assets/Script/GameManager.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public bool IsGameOver;
+    public GameObject obstaculo;
+
+    private float intervalo;
+    public float delay = 3;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        intervalo = Time.time + delay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RespawnManager();
     }
-}
+
+    public void RespawnManager()
+    {
+        if(intervalo <= Time.time && IsGameOver == false)
+        {
+            Instantiate(obstaculo, new Vector3(10, Random.Range(-3, 6),0), Quaternion.identity);
+            intervalo = Time.time + delay;
+        }
+    }
+} 
+
