7d8003a991037eca7a9919802713dc017db3e4ca
Pedro Henrique de Oliveira Andrade <pedro.andrade9@aluno.senai.br>
2023-09-18 11:50:53 -0300

    Atualização 18/09/2023

--- FlappyBird/Assets/Script/EnemyMoves.cs
+++ FlappyBird/Assets/Script/EnemyMoves.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class EnemyMoves : MonoBehaviour
 {
+    private Rigidbody2D enemyRb;
+    public float speed, speedY,posicao69,altura,alturaMax;
+    public Vector2 posTop,posBot,posToGo;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        enemyRb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x >= posicao69)
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+
+       if(transform.position.x <= posicao69)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, posToGo, speedY);
+        }
+
+        if(transform.position.y >= posTop.y)
+        {
+            posToGo = posBot;
+        }
+        else if (transform.position.y <= posBot.y)
+        {
+            posToGo = posTop;
+        }
         
     }
 }
